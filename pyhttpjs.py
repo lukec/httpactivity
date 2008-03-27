84a2d88b99332a0070fa5e7df0ba1a4995f37d2e
Luke Closs <olpc@5thplane.com>
2008-03-27 15:17:11 -0400

    combine http server and browser, remove xpcom

--- pyhttpjs.py
+++ pyhttpjs.py
@@ -1,18 +1,63 @@
 #!/usr/bin/python
-import SimpleHTTPServer
 import BaseHTTPServer
+import thread
+import httplib
+from time import sleep
+import os
+import gtk
+import hulahop
+hulahop.startup(os.path.expanduser('~/.test-hulahop'))
+from hulahop.webview import WebView
 
-class SocialcalcRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class SocialcalcRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def init (self, request, client_address, server):
-        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, 
+        BaseHTTPServer.BaseHTTPRequestHandler.__init__(self, request, 
                                                            client_address, server)
 
     def do_GET(self):
-        print "do_GET\n";
+        self.send_response(200, "Awesome")
 
-# server = SimpleHTTPServer(("", 4080), SocialcalcRequestHandler)
-port = 4080
-print "Port: %s"%port
-server = BaseHTTPServer.HTTPServer(("", port), SocialcalcRequestHandler)
-server.serve_forever()
+class SocialCalcServer:
+    def __init__(self):
+        self.port = 4080
 
+    def start(self):
+        try:
+            t = thread.start_new(
+                    self._socialcalc_server,
+                    ())
+        except Exception, e:
+            print "EXCEPTION: " + str(e)
+        self._server_thread = t
+        print "Started thread"
+
+    def _socialcalc_server(self, *args, **keys):
+        print "Starting HTTP Server on port: %s"%self.port
+        server = BaseHTTPServer.HTTPServer(("", self.port), SocialcalcRequestHandler)
+        server.serve_forever()
+
+
+s = SocialCalcServer()
+s.start()
+
+
+def quit(window):
+    hulahop.shutdown()
+    gtk.main_quit()
+
+def keypress(window, key):
+    print "pressed: " + key.string
+
+window = gtk.Window()
+window.set_default_size(600, 400)
+window.connect("destroy", quit)
+window.connect("key_press_event", keypress)
+
+web_view = WebView()
+web_view.load_uri('file:///home/olpc/src/pyhttpjs/index.html')
+web_view.show()
+
+window.add(web_view)
+window.show()
+
+gtk.main()
