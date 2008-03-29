b7ffcc5c8a8a679fffe367d417ed4b7d82046d25
Luke Closs <olpc@5thplane.com>
2008-03-29 17:59:15 -0400

    poll the server for commands

--- xo-code.js
+++ xo-code.js
@@ -1,1 +1,20 @@
-alert('poop');
+jQuery(function () {
+    poller = function () {
+        $.getJSON("/status.json", function(json){
+            $("<p>Command:" + json.command + "</p>").appendTo("body")
+            if (json.command == "read") {
+                document.XO_read_hook(json.content)
+            }
+            else if (json.command == "write") {
+                var save_content = document.XO_write_hook()
+                $.post("/write", { "content" : save_content })
+            }
+            else {
+                $("<p>Unknown</p>").appendTo("body")
+            }
+        })
+    }
+
+    setInterval( "poller()", 5000 )
+})
+
